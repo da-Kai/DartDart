c143bf97cea773cf39c15d6cadff7ab4bde305ca
da-Kai <kaijeschek+github@gmail.com>
2023-12-15 11:52:09 +0100

    add DarkMode ColorScheme;

--- lib/constants/color.dart
+++ lib/constants/color.dart
@@ -11,53 +11,50 @@ Color hexToColor(String hex) {
 class ColorConstants {
   ColorConstants._();
 
-  static Color primary = const Color.fromRGBO(2, 88, 155, 1.0);
-  static Color primaryLight = const Color.fromRGBO(12, 127, 206, 1.0);
-  static Color primaryShade = const Color.fromRGBO(1, 63, 126, 1.0);
-  static Color primaryContrast = const Color.fromRGBO(62, 119, 189, 1.0);
+  static const Color primary = Color.fromRGBO(2, 88, 155, 1.0);
+  static const Color primaryLight = Color.fromRGBO(12, 127, 206, 1.0);
+  static const Color primaryShade = Color.fromRGBO(1, 63, 126, 1.0);
+  static const Color primaryContrast = Color.fromRGBO(62, 119, 189, 1.0);
 
-  static Color secondary = const Color.fromRGBO(44, 183, 246, 1.0);
-  static Color secondaryLight = const Color.fromRGBO(104, 210, 248, 1.0);
-  static Color secondaryShade = const Color.fromRGBO(0, 148, 245, 1.0);
+  static const Color secondary = Color.fromRGBO(44, 183, 246, 1.0);
+  static const Color secondaryLight = Color.fromRGBO(104, 210, 248, 1.0);
+  static const Color secondaryShade = Color.fromRGBO(0, 148, 245, 1.0);
 
-  static Color success = const Color.fromRGBO(137, 206, 148, 1.0);
-  static Color warning = const Color.fromRGBO(254, 186, 83, 1.0);
-  static Color fault = const Color.fromRGBO(224, 122, 95, 1.0);
+  static const Color success = Color.fromRGBO(137, 206, 148, 1.0);
+  static const Color warning = Color.fromRGBO(254, 186, 83, 1.0);
+  static const Color fault = Color.fromRGBO(224, 122, 95, 1.0);
 
-  static Color text = const Color.fromRGBO(5, 10, 20, 1.0);
-  static Color background = const Color.fromRGBO(240, 240, 250, 1.0);
-  static Color shadow = const Color.fromRGBO(5, 10, 20, 0.3);
-
-  static Color unset = const Color.fromRGBO(20, 255, 20, 1.0);
+  static const Color text = Color.fromRGBO(5, 10, 20, 1.0);
+  static const Color background = Color.fromRGBO(240, 240, 250, 1.0);
+  static const Color shadow = Color.fromRGBO(5, 10, 20, 0.3);
 }
 
 class ColorSchemes {
-  static ColorScheme light = ColorScheme(
-    brightness: Brightness.light,
+  static const ColorScheme light = ColorScheme.light(
     primary: ColorConstants.primary,
     onPrimary: ColorConstants.text,
     secondary: ColorConstants.secondary,
     onSecondary: ColorConstants.text,
     tertiary: ColorConstants.secondary,
     onTertiary: ColorConstants.text,
-    shadow: ColorConstants.shadow,
     error: ColorConstants.fault,
     onError: ColorConstants.text,
     errorContainer: ColorConstants.fault,
     onErrorContainer: ColorConstants.text,
-    background: ColorConstants.background,
-    onBackground: ColorConstants.text,
-    /* unset values */
-    surface: ColorConstants.unset,
-    onSurface: ColorConstants.text,
-    surfaceVariant: ColorConstants.unset,
-    surfaceTint: ColorConstants.unset,
-    onSurfaceVariant: ColorConstants.unset,
-    outline: ColorConstants.unset,
-    outlineVariant: ColorConstants.unset,
-    scrim: ColorConstants.unset,
-    inverseSurface: ColorConstants.unset,
-    onInverseSurface: ColorConstants.unset,
-    inversePrimary: ColorConstants.unset,
+    //background: ColorConstants.background,
+    //onBackground: ColorConstants.text,
+  );
+
+  static const ColorScheme dark = ColorScheme.dark(
+    primary: ColorConstants.primary,
+    onPrimary: ColorConstants.text,
+    secondary: ColorConstants.secondary,
+    onSecondary: ColorConstants.text,
+    tertiary: ColorConstants.secondary,
+    onTertiary: ColorConstants.text,
+    error: ColorConstants.fault,
+    onError: ColorConstants.text,
+    errorContainer: ColorConstants.fault,
+    onErrorContainer: ColorConstants.text,
   );
 }
