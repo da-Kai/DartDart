29cb8d89af377e6bc88728ee03e54a0f1701cc82
da-Kai <kaijeschek+github@gmail.com>
2024-06-14 18:33:35 +0200

    quickfix

--- lib/pages/games/x01_game_page.dart
+++ lib/pages/games/x01_game_page.dart
@@ -312,7 +312,7 @@ class _CurrentPlayer extends StatelessWidget {
                   style: titleStyle,
                 ),
                 const Spacer(flex: 2),
-                game.curTurn.isWin
+                game.curTurn.isCheckout
                     ? //
                     Icon(Icons.check, color: colorScheme.success)
                     : //
