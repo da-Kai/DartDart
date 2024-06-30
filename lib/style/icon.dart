b2a02fc6ddcb5fbe25e93dd40f8eadbf95a63302
da-Kai <kaijeschek+github@gmail.com>
2024-06-30 21:55:30 +0200

    Add Icons to Font

--- lib/style/icon.dart
+++ lib/style/icon.dart
@@ -6,6 +6,10 @@ class CustomIcons {
 
   static const _fontFamily = 'dartdart';
 
-  static const IconData dartDart = IconData(0xe801, fontFamily: _fontFamily);
-  static const IconData github = IconData(0xf308, fontFamily: _fontFamily);
+  static const IconData dartDart = IconData(0xe800, fontFamily: _fontFamily);
+  static const IconData github = IconData(0xe801, fontFamily: _fontFamily);
+
+  static const IconData oneLeg = IconData(0xe802, fontFamily: _fontFamily);
+  static const IconData twoLegs = IconData(0xe803, fontFamily: _fontFamily);
+  static const IconData threeLegs = IconData(0xe804, fontFamily: _fontFamily);
 }
