ce8f3bb15efadc37c7b044dc4ba08432b26d573b
Kai J. <da.kai.creator@gmail.com>
2025-02-22 20:24:16 +0100

    update

--- lib/pages/games/x01_game_statistics.dart
+++ lib/pages/games/x01_game_statistics.dart
@@ -1,5 +1,6 @@
 import 'package:dart_dart/logic/x01/game.dart';
 import 'package:dart_dart/logic/x01/settings.dart';
+import 'package:dart_dart/style/color.dart';
 import 'package:flutter/material.dart';
 
 import '../../style/font.dart';
@@ -43,7 +44,7 @@ class _X01StatsState extends State<X01Statistics> {
             ),
             centerTitle: true,
           ),
-          body: _TableView(this, setState)
+          body: _PlayerStatsView(this, setState)
         ));
   }
 }
@@ -88,5 +89,124 @@ class _TableView extends StatelessWidget {
         )
       ],
     );
+  }
+}
+
+class _PlayerStatsView extends StatelessWidget {
+  final _X01StatsState state;
+  final Function update;
+
+  const _PlayerStatsView(this.state, this.update);
+
+  @override
+  Widget build(BuildContext context) {
+    final GameStats data = state.widget.stats;
+    final ColorScheme colorScheme = Theme.of(context).colorScheme;
+    final TextStyle textStyle =
+      FontConstants.subtitle.copyWith(color: colorScheme.onPrimaryContainer);
+
+    return Row(
+      children: [
+        Container(
+          padding: const EdgeInsets.all(10.0),
+          margin: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20),
+                bottomLeft: Radius.circular(20),
+            ),
+            color: colorScheme.backgroundShade,
+          ),
+          child: _StatsColumn(
+            name: 'Player',
+            avg: 'AVG',
+            max: 'MAX',
+            min: 'MIN',
+            most: 'Most-Hits',
+            sixtyPlus: '60+',
+            oneTwentyPlus: '120+',
+            oneEighty: '180'
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(10.0),
+          margin: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 5.0),
+          decoration: BoxDecoration(
+            color: colorScheme.backgroundShade,
+          ),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  for (final player in data.playerStats.entries)
+                    _StatsColumn(
+                        name: player.key,
+                        avg: player.value.avgScore.toStringAsFixed(2),
+                        max: player.value.maxPoints.toString(),
+                        min: player.value.minPoints.toString(),
+                        most: player.value.mostHit.abbreviation,
+                        sixtyPlus: player.value.sixtyPlusCnt.toString(),
+                        oneTwentyPlus: player.value.oneTwentyPlusCnt.toString(),
+                        oneEighty: player.value.oneEightyCnt.toString()
+                    ),
+                ],
+              )
+            ],
+          )
+        ),
+      ],
+    );
+  }
+}
+
+class _StatsColumn extends StatelessWidget {
+
+  final String name;
+  final String avg;
+  final String max;
+  final String min;
+  final String most;
+  final String sixtyPlus;
+  final String oneTwentyPlus;
+  final String oneEighty;
+
+  const _StatsColumn({required this.name, required this.avg, required this.max, required this.min, required this.most, required this.sixtyPlus, required this.oneTwentyPlus, required this.oneEighty});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Container(
+        padding: const EdgeInsets.symmetric(vertical: 5.0),
+        child: Text(name),
+      ),
+      Container(
+        padding: const EdgeInsets.only(top: 20.0, bottom: 5.0),
+        child: Text(avg),
+      ),
+      Container(
+        padding: const EdgeInsets.symmetric(vertical: 5.0),
+        child: Text(max),
+      ),
+      Container(
+        padding: const EdgeInsets.symmetric(vertical: 5.0),
+        child: Text(min),
+      ),
+      Container(
+        padding: const EdgeInsets.only(top: 20.0, bottom: 5.0),
+        child: Text(most),
+      ),
+      Container(
+        padding: const EdgeInsets.only(top: 20.0, bottom: 5.0),
+        child: Text(sixtyPlus),
+      ),
+      Container(
+        padding: const EdgeInsets.symmetric(vertical: 5.0),
+        child: Text(oneTwentyPlus),
+      ),
+      Container(
+        padding: const EdgeInsets.symmetric(vertical: 5.0),
+        child: Text(oneEighty),
+      ),
+    ]);
   }
 }
