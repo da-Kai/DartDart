93277e014e11d1bd6bbafb60d991a769a0e82bcf
Kai J. <da.kai.creator@gmail.com>
2025-03-05 21:48:22 +0100

    create junit.xml report for tests

---
+++ scripts/flutter-test.sh
@@ -0,0 +1,22 @@
+flutter pub get junitreport
+export PATH="$PATH":"$HOME/.pub-cache/bin"
+flutter pub global activate junitreport
+
+junitFile="./junit.xml"
+flutter test --machine --coverage --coverage-path=lcov.info | tojunit --output $junitFile
+
+errors=$(xmlstarlet sel -t -v "sum(//testsuite/@errors)" "$junitFile")
+failures=$(xmlstarlet sel -t -v "sum(//testsuite/@failures)" "$junitFile")
+tests=$(xmlstarlet sel -t -v "sum(//testsuite/@tests)" "$junitFile")
+skipped=$(xmlstarlet sel -t -v "sum(//testsuite/@skipped)" "$junitFile")
+
+if [[ "$errors" == "0" && "$failures" == "0" ]]; then
+    echo " All Tests Passed "
+    STATUS=0
+else
+    echo " Some Test Failed "
+    STATUS=1
+fi
+
+echo "Tests: $tests, Skipped: $skipped, Errors: $errors, Failures: $failures"
+exit $STATUS
