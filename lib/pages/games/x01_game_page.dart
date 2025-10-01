129d1aad661856cdfa9c9df0bb421a4551d41997
devalgupta404 <devalgupta4@gmail.com>
2025-10-02 02:19:03 +0530

    Undo on back gesture (#80)

--- lib/pages/games/x01_game_page.dart
+++ lib/pages/games/x01_game_page.dart
@@ -40,6 +40,16 @@ class _X01PageState extends State<X01Game> {
 
     return PopScope(
         canPop: false,
+        onPopInvokedWithResult: (didPop, result) {
+          if (didPop) {
+            return;
+          }
+          if (data.canUndo) {
+            setState(() {
+              data.undo();
+            });
+          }
+        },
         child: Scaffold(
           resizeToAvoidBottomInset: false,
           appBar: AppBar(
